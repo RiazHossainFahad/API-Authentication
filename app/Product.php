8cbcaf32f24afc75b2d7e40cf4fd4a73d0d54141
Md. Riaz Hossain Fahad <mdriazhossainfahad@gmail.com>
2019-11-20 17:04:10 +0600

    Factory, DB seed completed, relationship are build

--- app/Product.php
+++ app/Product.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use App\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function review()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Product;
+use App\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(Product::class, 50)->create();
+        factory(Review::class, 300)->create();
     }
 }
